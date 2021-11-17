d168ceee96b5cc127d64077cfe0e4b74d4f9e90c
Nico Derp <derp.nico@gmail.com>
2021-11-17 23:27:54 +0100

    Added two player support and proper collision.

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,55 @@ use piston_window::*;
 struct Ball {
 	x: f64,
 	y: f64,
+	r: f64,
 	c: f64,
 	s: f64,
 	angle: f64,
 	speed: f64,
 }
 
+struct Player {
+	y: f64,
+	key_move: f64,
+}
+
 fn main() {
 	let window_x = 640;
 	let window_y = 480;
-	let pi = 3.14;
-	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (window_x, window_y)).exit_on_esc(true).build().unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
-
-	let mut player1_y = 0.0;
-	let mut player2_y = 0.0;
 	
-	let mut ball = Ball {
-		x: 100.0,
-		y: 100.0,
-		c: 0.0,
-		s: 0.0,
-		angle: pi/3.0,
-		speed: 1.0,
+	let pi = 3.14;
+	let two_pi = 2.0*pi;
+
+	let mut window: PistonWindow = WindowSettings::new("Pong!", (window_x, window_y))
+		.exit_on_esc(true)
+		.build()
+		.unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+
+	let mut player1 = Player {
+		y: 0.0,
+		key_move: 0.0,
 	};
 
+	let mut player2 = Player {
+		y: 0.0,
+		key_move: 0.0,
+	};
+	
+	let mut ball = Ball {
+		x: 400.0,
+		y: 100.0,
+		r: 40.0,
+		c: 0.0,
+		s: 0.0,
+		angle: pi*1.2,
+		speed: 0.3,
+	};
+
+	let player_speed = 1.0;
+	let player_width = 20.0;
+	let player_height = 50.0;
+	let player_space = 20.0;
+	
 	ball.c = ball.angle.cos();
 	ball.s = ball.angle.sin();
 	
@@ -37,48 +62,101 @@ fn main() {
 			clear([0.1, 0.1, 0.1, 1.0], g);
 
 			// Player 1
-			rectangle([1.0, 0.0, 0.0, 1.0], [20.0, player1_y, 20.0, 50.0], c.transform, g);
+			rectangle([1.0, 0.0, 0.0, 1.0], [player_space, player1.y, player_width, player_height], c.transform, g);
 
 			// Player 2
-			rectangle([1.0, 0.0, 0.0, 1.0], [window_x as f64 - (20.0 + 20.0), player2_y, 20.0, 50.0], c.transform, g);
+			rectangle([1.0, 0.0, 0.0, 1.0], [window_x as f64 - (player_width + player_space), player2.y, player_width, player_height], c.transform, g);
 
 			// Ball
-			ellipse([0.9, 0.9, 0.9, 1.0], [ball.x, ball.y, 40.0, 40.0], c.transform, g);
+			ellipse([0.9, 0.9, 0.9, 1.0], [ball.x, ball.y, ball.r, ball.r], c.transform, g);
 		});
 
-		println!("had: {:?}", e.press_args());
+		if !e.press_args().is_none() {
+			let k = e.press_args().unwrap();
 
-		if let Some(Button::Keyboard(key)) = e.press_args() {
-			if key == Key::W {
-				println!("Pressed keyboard key '{:?}'", key);
+			match k {
+				Button::Keyboard(Key::W) => player1.key_move = -1.0,
+				Button::Keyboard(Key::S) => player1.key_move = 1.0,
+
+				Button::Keyboard(Key::O) => player2.key_move = -1.0,
+				Button::Keyboard(Key::L) => player2.key_move = 1.0,
+				_ => (),
 			}
+			
+			println!("Key pressed: {:?}", k);
 		}
 
-		//let b = Input::Button(ButtonArgs{
-		//	state: ButtonState::Press,
-		//	button: Button::Keyboard(Key::W), // key
-		//	scancode: Some(11),
-		//});
+		if !e.release_args().is_none() {
+			let k = e.release_args().unwrap();
 
-		//let b = Input::Text(String::from("W"));
-		//let mut evt = Event::Input(b, Some(0));
-		// let mut evt = Event::Input(Input::Text(String::from("W")), Some(0));
+			match k {
+				Button::Keyboard(Key::W) => player1.key_move = 0.0,
+				Button::Keyboard(Key::S) => player1.key_move = 0.0,
 
-		//let a = ButtonEvent::button(&evt, keyPressed);
-		//let mut a = ButtonEvent::button_args(&evt);
-		//if (!a.is_none()) {
-		//	print!("aasdasd");
-		//	print!("{}", a.unwrap().scancode.unwrap());
-		//}
+				Button::Keyboard(Key::O) => player2.key_move = 0.0,
+				Button::Keyboard(Key::L) => player2.key_move = 0.0,
+				_ => (),
+			}
+			
+			println!("Key released: {:?}", k);
+		}
+
+		println!("Player 1: {}", player1.key_move);
+
+		player1.y += player1.key_move * player_speed;
+		player2.y += player2.key_move * player_speed;
 		
 		ball.x += ball.c * ball.speed;
 		ball.y += ball.s * ball.speed;
+
+		let mut collided = false;
+		println!("ball y: {}", ball.y);
+
+		// Player 1
+		if (ball.x <= player_space + player_width) {
+			if (player1.y <= ball.y && ball.y <= (player1.y + player_height)) || (player1.y <= (ball.y + ball.r) && (ball.y + ball.r) <= (player1.y + player_height)) {
+				ball.angle = pi - ball.angle;
+				collided = true;
+				ball.speed += 0.2;
+			}
+		}
+
+		// Player 2
+		if (ball.x >= (window_x as f64) - player_space - player_width - ball.r) {
+			if (player2.y <= ball.y && ball.y <= (player2.y + player_height)) || (player2.y <= (ball.y + ball.r) && (ball.y + ball.r) <= (player2.y + player_height)) {
+				ball.angle = pi - ball.angle;
+				collided = true;
+				ball.speed += 0.2;
+			}
+		}
+
+		// Top
+		if ball.y <= 0.0 {
+			ball.angle = two_pi - ball.angle;
+			collided = true;
+		}
+
+		// Bottom
+		if ball.y + ball.r >= window_y.into() {
+			ball.angle = two_pi - ball.angle;
+			collided = true;
+		}
+
+		// Right
+		if ball.x + ball.r >= window_x.into() {
+			println!("Player 1 Won!");
+		}
+
+		// Left
+		if ball.x <= 0.0 {
+			println!("Player 2 Won!");
+		}
+
+		// Calculate new values
+		if collided {
+			println!("Calculating new values...");
+			ball.c = ball.angle.cos();
+			ball.s = ball.angle.sin();
+		}
 	}
 }
-
-fn keyPressed(args: ButtonArgs) {
-	print!("Key state: {}", args.state == ButtonState::Release);
-}
-
-// https://crates.io/crates/piston
-
