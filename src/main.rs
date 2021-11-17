db82a9b7d31e6aeb52cc7259be6a7f880c4ddcee
Nico Derp <derp.nico@gmail.com>
2021-11-17 13:51:49 +0100

    Correctly rendering ball and players

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
 extern crate piston_window;
 use piston_window::*;
-fn main() {
-	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480)).exit_on_esc(true).build().unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
 
-	let mut x = 0.0;
-	let mut y = 0.0;
+struct Ball {
+	x: f64,
+	y: f64,
+	c: f64,
+	s: f64,
+	angle: f64,
+	speed: f64,
+}
+
+fn main() {
+	let window_x = 640;
+	let window_y = 480;
+	let pi = 3.14;
+	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (window_x, window_y)).exit_on_esc(true).build().unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+
+	let mut player1_y = 0.0;
+	let mut player2_y = 0.0;
+	
+	let mut ball = Ball {
+		x: 100.0,
+		y: 100.0,
+		c: 0.0,
+		s: 0.0,
+		angle: pi/3.0,
+		speed: 1.0,
+	};
+
+	ball.c = ball.angle.cos();
+	ball.s = ball.angle.sin();
+	
+	// print!("{}", angle.sin());
 	while let Some(e) = window.next() {
 		window.draw_2d(&e, |c, g, _d| {
-			clear([0.5, 1.0, 0.5, 1.0], g);
-			
-			rectangle([1.0, 0.0, 0.0, 1.0], [x, y, 50.0, 50.0], c.transform, g);
+			clear([0.1, 0.1, 0.1, 1.0], g);
+
+			// Player 1
+			rectangle([1.0, 0.0, 0.0, 1.0], [20.0, player1_y, 20.0, 50.0], c.transform, g);
+
+			// Player 2
+			rectangle([1.0, 0.0, 0.0, 1.0], [window_x as f64 - (20.0 + 20.0), player2_y, 20.0, 50.0], c.transform, g);
+
+			// Ball
+			ellipse([0.9, 0.9, 0.9, 1.0], [ball.x, ball.y, 40.0, 40.0], c.transform, g);
 		});
-		x+=0.2;
-		y+=0.2;
+		
+		ball.x += ball.c * ball.speed;
+		ball.y += ball.s * ball.speed;
 	}
 }
 
