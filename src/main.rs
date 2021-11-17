458a3af381a7a049b83693d22e20b92e5bdbedb7
Nico Derp <derp.nico@gmail.com>
2021-11-17 12:35:16 +0100

    Square demo working.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ extern crate piston_window;
 use piston_window::*;
 fn main() {
 	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480)).exit_on_esc(true).build().unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+
+	let mut x = 0.0;
+	let mut y = 0.0;
 	while let Some(e) = window.next() {
-		window.draw_2d(&e, |_c, g, _d| {
+		window.draw_2d(&e, |c, g, _d| {
 			clear([0.5, 1.0, 0.5, 1.0], g);
+			
+			rectangle([1.0, 0.0, 0.0, 1.0], [x, y, 50.0, 50.0], c.transform, g);
 		});
+		x+=0.2;
+		y+=0.2;
 	}
 }
 
