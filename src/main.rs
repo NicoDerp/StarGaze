d91728a3c80b5055e83c5d85231dab574a654648
Nico Derp <derp.nico@gmail.com>
2021-11-17 11:33:09 +0100

    Updated dependencies.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate piston_window;
 use piston_window::*;
 
 fn main() {
-	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+	let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480]);
 }
 
+
 // https://crates.io/crates/piston
+
